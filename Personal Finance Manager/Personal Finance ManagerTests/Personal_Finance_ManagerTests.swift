38a3be05605d138c7d49b0ccf710f31f171cff9f
guilherme <jguicf@gmail.com>
2026-02-23 20:32:47 -0400

    Configuring xcode project

---
+++ Personal Finance Manager/Personal Finance ManagerTests/Personal_Finance_ManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Personal_Finance_ManagerTests.swift
+//  Personal Finance ManagerTests
+//
+//  Created by João Guilherme on 23/02/26.
+//
+
+import Testing
+@testable import Personal_Finance_Manager
+
+struct Personal_Finance_ManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
